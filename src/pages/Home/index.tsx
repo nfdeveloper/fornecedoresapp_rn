e8895bbd3c4bd1aeb92db5c64be060edba3f95ba
nfdeveloper <nfdeveloper04@gmail.com>
2023-06-10 12:24:02 -0300

    Criando aplicação e configurando styled components.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { Container, Title } from "./styles"
+
+export const Home: React.FunctionComponent = () =>{
+    return (
+        <Container>
+            <Title>Home Page</Title>
+        </Container>
+    )
+}
